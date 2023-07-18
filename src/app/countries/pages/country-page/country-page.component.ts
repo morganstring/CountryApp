cdeadae12772ada3e84774461535c86933ad66a1
Cristina Garcia <morganstring@hotmail.com>
2023-07-18 14:04:41 +0200

    126.Navegar hacia la pagina del pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +10,28 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent  implements OnInit {
+
+  public country?: Country;
+
+  constructor (
+     private activatedRoute: ActivatedRoute,
+     private CountriesService: CountriesService, 
+     private router: Router,
+     ) {}
+  ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap ( ( { id }) => this.CountriesService.searchCountryAlphaCode ( id) ),
+      )
+    .subscribe (  country  => { 
+      if ( !country ) return this.router.navigateByUrl ('');
+      return this.country = country; 
+     
+      });
+  }
+
+
 
 }
